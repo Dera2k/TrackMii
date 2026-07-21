e4934707c684755d68a481b82848347e2200a056
Nwogu Chidera <chideranwogu2003@gmail.com>
2026-07-21 21:00:26 +0100

    feat: Configure TypeORM for Supabase PostgreSQL

--- trackmii-backend/src/config/database.config.ts
+++ trackmii-backend/src/config/database.config.ts
@@ -3,16 +3,18 @@ import { ConfigService } from '@nestjs/config';
 
 export const getDatabaseConfig = (
   configService: ConfigService,
-  
 ): TypeOrmModuleOptions => ({
   type: 'postgres',
   host: configService.get('DB_HOST'),
-  port: configService.get('DB_PORT'),
+  port: Number(configService.get('DB_PORT')),
   username: configService.get('DB_USERNAME'),
   password: configService.get('DB_PASSWORD'),
   database: configService.get('DB_DATABASE'),
+  ssl: {
+    rejectUnauthorized: false,
+  },
   autoLoadEntities: true,
   migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
   synchronize: configService.get('NODE_ENV') === 'development',
-  logging: false //configService.get('NODE_ENV') === 'development',
+  logging: false,
 });
